ea0199cbdc3cba2215be2ba19b0360ea4e28de6a
brendanobilo <brendanxure@outlook.com>
2026-08-28 21:26:06 -0400

    initial commmit

---
+++ src/main/java/com/example/securitydemo/service/AdminUserInitializer.java
@@ -0,0 +1,32 @@
+package com.example.securitydemo.service;
+
+import com.example.securitydemo.entity.Users;
+import com.example.securitydemo.repository.UserDetailsRepository;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Component;
+
+@Component
+public class AdminUserInitializer {
+    @Bean
+    public CommandLineRunner createAdminUser(
+            UserDetailsRepository userRepository,
+            PasswordEncoder passwordEncoder) {
+
+        return args -> {
+
+            if (userRepository.findByUsername("admin").isEmpty()) {
+
+                Users admin = new Users();
+
+                admin.setUsername("admin");
+                admin.setPassword(passwordEncoder.encode("admin123"));
+                admin.setRole("ADMIN");
+
+                userRepository.save(admin);
+                System.out.println("Default admin user created");
+            }
+        };
+    }
+}
